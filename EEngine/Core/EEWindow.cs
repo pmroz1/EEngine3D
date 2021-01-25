fb3da24bfb7e5bbc6dfe9ba28a555d766e969c39
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-01-25 14:53:08 +0100

    initialized engine and demo game

---
+++ EEngine/Core/EEWindow.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace EEngine.Core
+{
+    public class EEWindow
+    {
+        public EEWindow(Boolean logEnabled)
+        {
+            if (!logEnabled) return;
+            
+            Console.WriteLine("EEWindow is connected");
+            Window win = new Window();
+        }
+    }
+}
---
+++ Runner3D/Runner3D.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EEngine.Core;
+
+
+namespace Runner3D
+{
+    public static class Runner3D
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            EEWindow window = new EEWindow(true);
+        }
+    }
+}
