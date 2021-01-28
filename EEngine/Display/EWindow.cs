c6c1731488b46cda06c3fdc3a9a9916d90dad973
ReformedMrozu <p.mroz.zywiol@gmail.com>
2021-01-28 10:55:54 +0100

    huh

--- EEngine/Display/EWindow.cs
+++ EEngine/Display/EWindow.cs
@@ -18,141 +18,52 @@ using System.Text;
 
 namespace EEngine.Display
 {
-    public class EWindow
+    public class EWindow : GameWindow
     {
-        private GameWindow _window;
-
-        //private bool _logEnabled;
-        private readonly GameInfo _gameInfo;
-        //private GraphicsMode gm;
-        private List<IActor> _actors;
-        
-        public EWindow(string gameTitle, int width, int height, GameInfo gameInfo)
+        public EWindow(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(
+            gameWindowSettings, nativeWindowSettings)
         {
-            _gameInfo = gameInfo;
-            _actors = new List<IActor>();
-            var nativeWindowSettings = new NativeWindowSettings()
-            {
-                Size = new Vector2i(_gameInfo.WindowParams.Width, _gameInfo.WindowParams.Height),
-                Title = _gameInfo.Title,
-            };
-            _window = new GameWindow(GameWindowSettings.Default, nativeWindowSettings);
-            _window.UpdateFrame += UpdateFrame;
-            _window.RenderFrame += RenderFrame;
-            _window.Load += OnLoad;
-            _window.Unload += OnUnload;
-
-            GL.Enable(EnableCap.Texture2D);
-            Console.WriteLine("EEWindow is connected");
-        }
-
-        private void OnLoad()
-        {
-            _window.VSync = VSyncMode.On;
-        }
-
-        private void RenderFrame(FrameEventArgs obj)
-        {
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            ClearBackground();
-            Console.WriteLine("Clearing Background");
-            foreach (IActor a in _actors)
-            {
-                a.Render();
-                Console.WriteLine("RenderingObject");
-            }
-            //without swapping we will have 2-3fps
-            _window.SwapBuffers();
-            //_window.SwapBuffers();
-        }
-
-        private void UpdateFrame(FrameEventArgs obj)
-        {
-            foreach (var actor in _actors)
-            {
-                actor.Update();
-            }
-        }
-
-        public void AddActor(Actor3D ac)
-        {
-            _actors.Add(ac);
-        }
-        
-        private void ClearBackground()
-        {
-            //clears all color pixels
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.ClearColor(Color.Bisque);
-            GL.MatrixMode(MatrixMode.Projection);
-            GL.LoadIdentity();
-            GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
-        }
-
-        public void Cube(float size)
-        {
-            // GL.Color3(col);
-            // GL.Begin(PrimitiveType.Quads);
-            // GL.Vertex3(x, y, z);
-            // GL.Vertex3(x + cx, y, z);
-            // GL.Vertex3(x + cx, y + cy, z);
-            // GL.Vertex3(x, y + cy, z);
-            // GL.End();
-
-
-            float[,] n = new float[,]
-            {
-                {-1.0f, 0.0f, 0.0f},
-                {0.0f, 1.0f, 0.0f},
-                {1.0f, 0.0f, 0.0f},
-                {0.0f, -1.0f, 0.0f},
-                {0.0f, 0.0f, 1.0f},
-                {0.0f, 0.0f, -1.0f}
-            };
-            int[,] faces = new int[,]
-            {
-                {0, 1, 2, 3},
-                {3, 2, 6, 7},
-                {7, 6, 5, 4},
-                {4, 5, 1, 0},
-                {5, 6, 2, 1},
-                {7, 4, 0, 3}
-            };
-            float[,] v = new float[8, 3];
-            int i;
-
-            v[0, 0] = v[1, 0] = v[2, 0] = v[3, 0] = -size / 2;
-            v[4, 0] = v[5, 0] = v[6, 0] = v[7, 0] = size / 2;
-            v[0, 1] = v[1, 1] = v[4, 1] = v[5, 1] = -size / 2;
-            v[2, 1] = v[3, 1] = v[6, 1] = v[7, 1] = size / 2;
-            v[0, 2] = v[3, 2] = v[4, 2] = v[7, 2] = -size / 2;
-            v[1, 2] = v[2, 2] = v[5, 2] = v[6, 2] = size / 2;
             
-            GL.Begin(BeginMode.Quads);
-            for (i = 5; i >= 0; i--)
-            {
-                GL.Normal3(ref n[i, 0]);
-                GL.Vertex3(ref v[faces[i, 0], 0]);
-                GL.Vertex3(ref v[faces[i, 1], 0]);
-                GL.Vertex3(ref v[faces[i, 2], 0]);
-                GL.Vertex3(ref v[faces[i, 3], 0]);
-            }
+        }
 
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            Title = "Runner 3D";
+            GL.ClearColor(Color.Aqua);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            SwapBuffers();
+            Matrix4 modelview = Matrix4.LookAt(Vector3.Zero, Vector3.UnitZ, Vector3.UnitY);
+ 
+            GL.MatrixMode(MatrixMode.Modelview);
+ 
+            GL.LoadMatrix(ref modelview);
+            GL.Begin(BeginMode.Triangles);
+            GL.Vertex3(-1.0f, -1.0f, 4.0f);
+ 
+            GL.Vertex3(1.0f, -1.0f, 4.0f);
+ 
+            GL.Vertex3(0.0f, 1.0f, 4.0f);
             GL.End();
         }
 
-        protected void OnUnload()
+        protected override void OnResize(ResizeEventArgs e)
         {
-            // Unbind all the resources by binding the targets to 0/null.
-            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
-            GL.BindVertexArray(0);
-            GL.UseProgram(0);
-            // GL.DeleteProgram(_shader.Handle);
-        }
-
-        public void Run()
-        {
-            _window.Run();
+            base.OnResize(e);
+            base.OnResize(e);
+ 
+            GL.Viewport(200, 200, 800, 480);
+ 
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView((float)Math.PI / 4, 800 / (float)480, 1.0f, 64.0f);
+ 
+            GL.MatrixMode(MatrixMode.Projection);
+ 
+            GL.LoadMatrix(ref projection);
         }
     }
 }
--- Runner3D/Runner3D.cs
+++ Runner3D/Runner3D.cs
@@ -3,6 +3,7 @@ using EEngine.Actors;
 using EEngine.Core;
 using EEngine.Display;
 using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
 
 
 namespace Runner3D
@@ -11,26 +12,27 @@ namespace Runner3D
     {
         public static void Main()
         {
-            Console.WriteLine("Hello World");
-            var info = new GameInfo(
-                GameType.Game3D, "Runner3D",
-                new UpdateParameters(24, 12),
-                new GameWindowParams(800, 480)
-            );
-            
-            
-            ActorCoords acPlayer = new ActorCoords(new Vector2[]
-            {
-                new Vector2(0, 0),
-                new Vector2(0.2f, 0),
-                new Vector2(0.2f, -0.3f),
-                new Vector2(0, -0.3f)
-            });
+            // Console.WriteLine("Hello World");
+            // var info = new GameInfo(
+            //     GameType.Game3D, "Runner3D",
+            //     new UpdateParameters(24, 12),
+            //     new GameWindowParams(800, 480)
+            // );
+            //
+            //
+            // ActorCoords acPlayer = new ActorCoords(new Vector2[]
+            // {
+            //     new Vector2(0, 0),
+            //     new Vector2(0.2f, 0),
+            //     new Vector2(0.2f, -0.3f),
+            //     new Vector2(0, -0.3f)
+            // });
 
-            Actor3D ac = new Actor3D(acPlayer, "camera");
-            EWindow game = new EWindow("Runner3D", 800, 480, info);
-            game.AddActor(ac);
-            game.Run();
+            using (EWindow win = new EWindow(GameWindowSettings.Default, NativeWindowSettings.Default))
+            {
+                win.Run();
+            }
+            
         }
     }
 }
